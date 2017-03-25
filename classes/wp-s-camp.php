b9c29ca1644249ad0370fc2508e4911005b5abe7
dungendevelop <anshu12251992@gmail.com>
2017-03-26 01:33:55 +0530

    images metabox added

--- classes/wp-s-camp.php
+++ classes/wp-s-camp.php
@@ -51,11 +51,66 @@ if(!class_exists('Wp_Slideshow_Class'))
      
         function wp_slide_my_display_callback( $post ) {
             // Display code/markup goes here. Don't forget to include nonces!
+            wp_enqueue_script( 'wp_slide_js', plugins_url( '../js/wp_slide.js' , __FILE__ ) );
+            wp_enqueue_style( 'wp_slide_css', plugins_url( '../css/wp_slide.css' , __FILE__ ) );
+            echo '<div id="camp_slider_images_box"><a class="button button-primary add_image">Add image</a>';
+            global $content_width, $_wp_additional_image_sizes;
+            $content_width = 254;
+            $image_ids = get_post_meta( $post->ID, '_listing_image_id', true );
+            print_r($image_ids);
+            if(!empty($image_ids)){
+               foreach($image_ids as $image_id){
+                    echo '<div class="listingimagediv cloned">';
+                
+                    $old_content_width = $content_width;
+                    $content_width = 254;
+                    if ( $image_id && get_post( $image_id ) ) {
+                        if ( ! isset( $_wp_additional_image_sizes['post-thumbnail'] ) ) {
+                            $thumbnail_html = wp_get_attachment_image( $image_id, array( $content_width, $content_width ) );
+                        } else {
+                            $thumbnail_html = wp_get_attachment_image( $image_id, 'thumbnail' );
+                        }
+                        if ( ! empty( $thumbnail_html ) ) {
+                            $content = $thumbnail_html;
+                            $content .= '<p class="hide-if-no-js"><a href="javascript:;" class="remove_listing_image_button button" >' . esc_html__( 'Remove listing image', 'text-domain' ) . '</a></p>';
+                            $content .= '<input type="hidden" class="upload_listing_image" name="_listing_cover_image[]" value="' . esc_attr( $image_id ) . '" /></div>';
+                        }
+                        $content_width = $old_content_width;
+                    } else {
+                        
+                        $content = '<img src="" style="width:' . esc_attr( $content_width ) . 'px;height:auto;border:0;display:none;" />';
+                        $content .= '<p class="hide-if-no-js"><a title="' . esc_attr__( 'Set listing image', 'text-domain' ) . '" href="javascript:;" class="button upload_listing_image_button" id="set-listing-image" data-uploader_title="' . esc_attr__( 'Choose an image', 'text-domain' ) . '" data-uploader_button_text="' . esc_attr__( 'Set listing image', 'text-domain' ) . '">' . esc_html__( 'Set listing image', 'text-domain' ) . '</a></p>';
+                        $content .= '<input type="hidden" class="upload_listing_image" name="_listing_cover_image[]" value="" /></div>';
+                    }
+                    echo $content;
+                }
+                echo '</div>'; 
+            }else {
+                echo '<div class="listingimagediv">';
+                $content = '<img src="" style="width:' . esc_attr( $content_width ) . 'px;height:auto;border:0;display:none;" />';
+                $content .= '<p class="hide-if-no-js"><a title="' . esc_attr__( 'Set listing image', 'text-domain' ) . '" href="javascript:;" class="button upload_listing_image_button" id="set-listing-image" data-uploader_title="' . esc_attr__( 'Choose an image', 'text-domain' ) . '" data-uploader_button_text="' . esc_attr__( 'Set listing image', 'text-domain' ) . '">' . esc_html__( 'Set listing image', 'text-domain' ) . '</a></p>';
+                $content .= '<input type="hidden" class="upload_listing_image" name="_listing_cover_image[]" value="" /></div></div>';
+                echo $content;
+            }
+            
+            
+           
         }
          
       
         function wp_slide_save_meta_box( $post_id ) {
             // Save logic goes here. Don't forget to include nonce checks!
+            if( isset( $_POST['_listing_cover_image'] ) ) {
+                $data = $_POST['_listing_cover_image'];
+                $image_ids = array();
+                foreach($data as $id){
+                    if(!empty($id)){
+                        $image_ids[]=$id;
+                    }
+                }
+                if(is_array($image_ids) && count($image_ids))
+                    update_post_meta( $post_id, '_listing_image_id', $image_ids );
+            }
         }
         
         public function activate(){
---
+++ js/wp_slide.js
@@ -0,0 +1,56 @@
+jQuery(document).ready(function($) {
+
+
+  $('.add_image').click(function(){
+    var cloned =  $('.listingimagediv:first').clone();
+    cloned.addClass('cloned');
+    $('#camp_slider_images_box').append(cloned);
+  });
+  // Uploading files
+  var file_frame;
+
+  $.fn.upload_listing_image = function( button ) {
+
+      // Create the media frame.
+      file_frame = wp.media.frames.file_frame = wp.media({
+        title: $( this ).data( 'uploader_title' ),
+        button: {
+          text: $( this ).data( 'uploader_button_text' ),
+        },
+        multiple: false
+      });
+
+      // When an image is selected, run a callback.
+      file_frame.on( 'select', function() {
+        var attachment = file_frame.state().get('selection').first().toJSON();
+        console.log(attachment.id);
+        button.parent().parent().find(".upload_listing_image").val(attachment.id);
+        button.parent().parent().find("img").attr('src',attachment.url);
+        button.parent().parent().find( 'img' ).show();
+        button.removeClass( 'upload_listing_image_button' );
+        button.addClass( 'remove_listing_image_button' );
+        button.parent().parent().find( '.remove_listing_image_button' ).text( 'Remove listing image' );
+      });
+
+      // Finally, open the modal
+      file_frame.open();
+  };
+
+  $('#camp_slider_images_box').on( 'click', '.upload_listing_image_button', function( event ) {
+      var $this = $(this);
+      event.preventDefault();
+
+      $.fn.upload_listing_image( $this );
+  });
+
+  $('#camp_slider_images_box').on( 'click', '.remove_listing_image_button', function( event ) {
+      event.preventDefault();
+      var $this =  $(this);
+      $this.parent().parent().find( '.upload_listing_image' ).val( '' );
+      $this.parent().parent().find( 'img' ).attr( 'src', '' );
+      $this.parent().parent().find( 'img' ).hide();
+      $( this ).removeClass(  'remove_listing_image_button' );
+      $( this ).addClass(  'upload_listing_image_button' );
+      $this.parent().parent().find( '.upload_listing_image_button' ).text( 'Set listing image' );
+  });
+});
